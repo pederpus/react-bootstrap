58388e73e6f9157c56b91766610988de21aecbb7
Matthew Smith <mtscout6@gmail.com>
2015-05-07 20:48:20 -0600

    Use webpack-dev-server instead of webpack-dev-middleware
    Will update changes files both server and client side without needing to
    restart.
    
    Resolves #623

--- docs/server.js
+++ docs/server.js
@@ -1,46 +1,48 @@
+import 'colors';
 import React from 'react';
 import express from 'express';
 import path from 'path';
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackConfigBuilder from '../webpack/webpack.config';
 import Router from 'react-router';
 import routes from './src/Routes';
+import httpProxy from 'http-proxy';
 
 const development = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 4000;
+
 let app = express();
 
 if (development) {
-  let webpackConfig = webpackConfigBuilder({
-    development: development,
-    docs: true
-  });
-  let publicPath = webpackConfig.output.publicPath;
+  let proxy = httpProxy.createProxyServer();
+  let webpackPort = process.env.WEBPACK_DEV_PORT;
+  let target = `http://localhost:${webpackPort}`;
 
-  webpackConfig.output.path = '/';
-  webpackConfig.output.publicPath = undefined;
-  webpackConfig.module.noParse = /babel-core\/browser/;
+  app.all('/assets/*', function (req, res) {
+    proxy.web(req, res, { target });
+  });
 
   app = app
-    .use(webpackMiddleware(webpack(webpackConfig), {
-      noInfo: false,
-      publicPath: publicPath,
-      stats: {
-          colors: true
-      }
-    }))
+    .use(function webpackCrossOrigin(req, res, next) {
+      res.header('Access-Control-Allow-Origin', target);
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+      next();
+    })
     .use(function renderApp(req, res) {
       Router.run(routes, req.url, Handler => {
         let html = React.renderToString(<Handler />);
         res.send(html);
       });
     });
+
+  proxy.on('error', function(e) {
+    console.log('Could not connect to webpack proxy'.red);
+    console.log(e.toString().red);
+  });
 } else {
   app = app
     .use(express.static(path.join(__dirname, '../docs-built')));
 }
 
 app
-  .listen(4000, function () {
-    console.log('Server started at http://localhost:4000');
+  .listen(port, function () {
+    console.log(`Server started at http://localhost:${port}`);
   });
